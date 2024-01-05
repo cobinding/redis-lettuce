35f43034a5e332301d13d582ca320bd8fee019ae
추세빈 <cntpqls0218@gmail.com>
2024-01-05 13:17:36 +0900

    초기세팅

---
+++ src/main/java/redis/lettuceproject/LettuceProjectApplication.java
@@ -0,0 +1,13 @@
+package redis.lettuceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettuceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LettuceProjectApplication.class, args);
+    }
+
+}
