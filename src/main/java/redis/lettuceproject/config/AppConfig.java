02fc3d6a40245b5f81ec1872706dc4ca3f74efcf
추세빈 <cntpqls0218@gmail.com>
2024-01-08 14:01:26 +0900

    RedisTeamplate을 통해 Redis에 데이터 저장하기

--- src/main/java/redis/lettuceproject/config/AppConfig.java
+++ src/main/java/redis/lettuceproject/config/AppConfig.java
@@ -2,12 +2,14 @@ package redis.lettuceproject.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.CacheProperties.Redis;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class AppConfig {
@@ -18,8 +20,25 @@ public class AppConfig {
     @Value("6379")
     private int port;
 
-
+    @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    /*
+    * public RedisTemplate<?, ?> redisTemplate() {
+    *   RedisTemplate<byte[], bytep[]> redisTemplate = new RedisTemplate<>();
+    *
+    *
+    * */
 }
---
+++ src/main/java/redis/lettuceproject/controller/RedisController.java
@@ -0,0 +1,27 @@
+package redis.lettuceproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+
+    @GetMapping("/{key}/{value}")
+    public String stringsSet(@PathVariable String key, @PathVariable String value) {
+        final ValueOperations<String, String> stringStringValueOperations = redisTemplate.opsForValue();
+
+        stringStringValueOperations.set(key, value); // redis set 명령어
+        System.out.println("stringStringValueOperations's key = " + stringStringValueOperations.get(key));
+        return "ok";
+    }
+}
